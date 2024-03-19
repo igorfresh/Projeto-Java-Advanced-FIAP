0779d408482bbb5cb1184d87d5dd4c4b2853532f
Igor Silva <igormiguel37@gmail.com>
2024-03-19 16:44:10 -0300

    Entrega de implementação de persistencia de dados usando JPA

--- src/main/java/br/com/fiap/sistoque/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/sistoque/controller/CategoriaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +20,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.sistoque.model.Categoria;
 import br.com.fiap.sistoque.repository.CategoriaRepository;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("categoria")
-@Slf4j
 public class CategoriaController {
 
     Logger log = LoggerFactory.getLogger(getClass());
