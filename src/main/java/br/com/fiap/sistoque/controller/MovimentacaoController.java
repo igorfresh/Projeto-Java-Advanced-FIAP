f77c007b2fb1e23f92e97a05724ceb580d6e3564
Igor Silva <igormiguel37@gmail.com>
2024-03-19 22:46:35 -0300

    Aula 19/03 - Spring

--- src/main/java/br/com/fiap/sistoque/controller/MovimentacaoController.java
+++ src/main/java/br/com/fiap/sistoque/controller/MovimentacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.sistoque.model.Movimentacao;
 import br.com.fiap.sistoque.repository.MovimentacaoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("movimentacao")
@@ -28,7 +29,7 @@ public class MovimentacaoController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public Movimentacao create(@RequestBody Movimentacao movimentacao){
+    public Movimentacao create(@RequestBody @Valid Movimentacao movimentacao){
         return repository.save(movimentacao);
     }
 
--- src/main/java/br/com/fiap/sistoque/model/Movimentacao.java
+++ src/main/java/br/com/fiap/sistoque/model/Movimentacao.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,11 +17,22 @@ import lombok.Data;
 
 public class Movimentacao {
     
+
     @Id @GeneratedValue(strategy = GenerationType.AUTO) //dizer qual a estratégia pra gerar a chave primária
     private Long id;
+
+    @NotBlank(message = "Descrição é obrigatório")
+    @Size(min = 3, max = 255, message = "Descrição deve ter pelo menos 3 caracteres")
     private String descricao;
+
+    @Positive(message = "O valor deve ser positivo")
     private BigDecimal valor;
+
     private LocalDate data;
+
+    //@TipoMovimentacao
     private String tipo; //ENTRADA OU SAÍDA
+    
+
 
 }
