d868b04a7879e9c0565b37b7a1f965d65d73c5bb
igorfresh <igormiguel37@gmail.com>
2024-03-26 17:16:28 -0300

    Bean validation i18n - parte 1

---
+++ src/main/java/br/com/fiap/sistoque/validation/TipoMovimentacao.java
@@ -0,0 +1,20 @@
+package br.com.fiap.sistoque.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TipoMovimentacaoValidator.class)
+public @interface TipoMovimentacao {
+
+    String message() default "{movimentacao.tipo.message}";
+
+    Class<?> groups() default { };
+
+    Class <? extends Payload> payload 
+}
